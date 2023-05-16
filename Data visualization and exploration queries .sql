cd783fbae9a0eeab437fc613ae6f894af178eb89
Sapan26 <133113411+Sapan26@users.noreply.github.com>
2023-05-16 10:50:10 +0530

    Data visualization and exploration queries .sql

--- Data visualization and exploration queries .sql
+++ Data visualization and exploration queries .sql
@@ -137,11 +137,11 @@ From PopvsVac
 
 --infection vs population
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  
 Max((total_cases/population))*100 as PercentPopulationInfected
 From Project..CovidDeaths
 --Where location like '%states%'
-Group by Location, Population, date
+Group by Location, Population
 order by PercentPopulationInfected desc
 
 
